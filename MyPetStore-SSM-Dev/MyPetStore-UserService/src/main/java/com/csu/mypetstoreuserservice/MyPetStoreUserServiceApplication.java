b4be5b3b3361f0c72968812fa2a0d020ee2a3a4b
ONeofSu <148432545+ONeofSu@users.noreply.github.com>
2025-11-20 11:13:23 +0800

    Cloud Demo

---
+++ MyPetStore-SSM-Dev/MyPetStore-UserService/src/main/java/com/csu/mypetstoreuserservice/MyPetStoreUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.csu.mypetstoreuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetStoreUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPetStoreUserServiceApplication.class, args);
+    }
+
+}
---
+++ MyPetStore-SSM-Dev/MyPetStore-UserService/src/test/java/com/csu/mypetstoreuserservice/MyPetStoreUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csu.mypetstoreuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetStoreUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
